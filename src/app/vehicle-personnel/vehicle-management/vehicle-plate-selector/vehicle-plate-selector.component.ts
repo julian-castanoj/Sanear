fe3784ec9de64106e7afa338e58db668332bb1c3
practica.acueducto <practica.acueducto@ingenieria.local>
2024-07-22 17:31:22 -0500

    Add vehicle

--- src/app/vehicle-personnel/vehicle-management/vehicle-plate-selector/vehicle-plate-selector.component.ts
+++ src/app/vehicle-personnel/vehicle-management/vehicle-plate-selector/vehicle-plate-selector.component.ts
@@ -1,13 +1,82 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges, Output, EventEmitter, forwardRef } from '@angular/core';
+import { SheetsService } from '../../services/sheets.service';
+import { FormsModule, NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vehicle-plate-selector',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './vehicle-plate-selector.component.html',
-  styleUrl: './vehicle-plate-selector.component.css'
+  styleUrl: './vehicle-plate-selector.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => VehiclePlateSelectorComponent),
+      multi: true
+    }
+  ]
 })
-export class VehiclePlateSelectorComponent {
+
+export class VehiclePlateSelectorComponent implements OnInit, OnChanges, ControlValueAccessor {
+  @Input() columnIndex: number = 0;
   @Input() ngModel: any;
   @Output() ngModelChange = new EventEmitter<any>();
+  options: string[] = [];
+
+  private onChange: (value: any) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  constructor(private sheetsService: SheetsService) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['columnIndex'] && changes['columnIndex'].currentValue !== undefined) {
+      this.loadData();
+    }
+  }
+
+  private loadData(): void {
+    if (this.columnIndex !== undefined) {
+      this.sheetsService.getDataForColumn(this.columnIndex).subscribe(
+        (data: string[]) => {
+          this.options = data;
+          if (this.options.length > 0) {
+            this.ngModel = this.options[0];
+            this.onChange(this.ngModel);
+            this.ngModelChange.emit(this.ngModel);
+          }
+        },
+        (error: any) => {
+          console.error('Error fetching column data:', error);
+        }
+      );
+    }
+  }
+
+  onSelectionChange(value: any): void {
+    this.ngModelChange.emit(value);
+    this.onChange(value);
+    this.onTouched();
+  }
+
+  // ControlValueAccessor methods
+  writeValue(value: any): void {
+    this.ngModel = value;
+  }
+
+  registerOnChange(fn: (value: any) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    // Implement if necessary
+  }
 }
