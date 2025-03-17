98887b35bd9d41f3b1fee23873a115e8bbc67d1e
julian-castanoj <50122146+julian-castanoj@users.noreply.github.com>
2025-03-17 12:31:32 -0500

    Update services.service.ts

--- src/app/personnel-consult/services/services.service.ts
+++ src/app/personnel-consult/services/services.service.ts
@@ -8,8 +8,8 @@ import { map, catchError } from 'rxjs/operators';
 })
 
 export class ServicesService {
-  private apiKey = '!UW#G%F_8qyIV_ISvYEaRaIoc_gDIU6KrjZZxTyvuH3!o9m8rS$Q7@K4ka9R%Eaz';
-  private connectionUrl = 'https://sheet.best/api/sheets/dbc7714b-6e74-48aa-bd56-ea05d9697e7c/tabs/registros';
+  private apiKey = 'eHXB2W$-OVw2ASxfbr%%MxQ66!v6uc9nwGl!n84y2h#MR5E1Rd6cpPKTcBLi-lBW';
+  private connectionUrl = 'https://sheet.best/api/sheets/f0fcc516-53be-4c4f-824d-c0bbcb02fffb/tabs/registros';
 
 
 
