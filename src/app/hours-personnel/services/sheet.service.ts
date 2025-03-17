82513de28823e2cbaa82b17cf1b8e33458b56f50
julian-castanoj <50122146+julian-castanoj@users.noreply.github.com>
2025-03-17 12:38:05 -0500

    Update sheet.service.ts

--- src/app/hours-personnel/services/sheet.service.ts
+++ src/app/hours-personnel/services/sheet.service.ts
@@ -10,8 +10,8 @@ import { DataStorageService } from './data-storage.service';
 })
 
 export class SheetsService {
-  private apiKey = '!UW#G%F_8qyIV_ISvYEaRaIoc_gDIU6KrjZZxTyvuH3!o9m8rS$Q7@K4ka9R%Eaz'; 
-  private connectionUrl = 'https://sheet.best/api/sheets/dbc7714b-6e74-48aa-bd56-ea05d9697e7c'; 
+  private apiKey = 'eHXB2W$-OVw2ASxfbr%%MxQ66!v6uc9nwGl!n84y2h#MR5E1Rd6cpPKTcBLi-lBW'; 
+  private connectionUrl = 'https://sheet.best/api/sheets/f0fcc516-53be-4c4f-824d-c0bbcb02fffb'; 
 
   constructor(
     private http: HttpClient,
