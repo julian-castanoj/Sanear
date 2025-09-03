1a76b902213fa3439a328203bdd7400f53d5942b
julian-castanoj <50122146+julian-castanoj@users.noreply.github.com>
2025-09-03 17:42:19 -0500

    Actualizar sheets.service.ts

--- src/app/vehicle-personnel/services/sheets.service.ts
+++ src/app/vehicle-personnel/services/sheets.service.ts
@@ -9,11 +9,13 @@ import { DataSharingService } from './data-sharing.service';
   providedIn: 'root'
 })
 export class SheetsService {
-  private apiKey = 'Sb$Ur08t_AZv8g%5sViTx4Gt8jrYyW8HpYJ$L5@t1Y!KxPWGGRzkpWTcSlhCuikH';
-  private connectionUrl = 'https://sheet.best/api/sheets/4e7adda9-ae54-4fff-825d-00a3bfe790aa/tabs/vehiculos';
+  private apiKey = 'uAM-8gJhDzeWx%splaFtIgMx-1i2XEkBATCzEv5ioade$Fbvm1$cbZUr0Fu4yEC0';
+  private connectionUrl = 'https://sheet.best/api/sheets/38bd223f-a35a-4ae4-9136-c9e570aac1e0/tabs/vehiculos';
 
-   //     https://api.sheetbest.com/sheets/4e7adda9-ae54-4fff-825d-00a3bfe790aa
-//    'Sb$Ur08t_AZv8g%5sViTx4Gt8jrYyW8HpYJ$L5@t1Y!KxPWGGRzkpWTcSlhCuikH';
+    /*
+  private apiKey = 'uAM-8gJhDzeWx%splaFtIgMx-1i2XEkBATCzEv5ioade$Fbvm1$cbZUr0Fu4yEC0';
+  private connectionUrl = '38bd223f-a35a-4ae4-9136-c9e570aac1e0';
+*/
 
   constructor(
     private http: HttpClient,
