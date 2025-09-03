1f8c94930f401e134540e0f2da8f7ebbb126206a
julian-castanoj <50122146+julian-castanoj@users.noreply.github.com>
2025-09-03 17:33:01 -0500

    Actualizar data-storage.service.ts

--- src/app/consolidated-fortnight/services/data-storage.service.ts
+++ src/app/consolidated-fortnight/services/data-storage.service.ts
@@ -12,16 +12,18 @@ import { ObservationEntry } from './data-sharing.service';
 })
 
 export class DataStorageService {
-  private apiKey = 'Sb$Ur08t_AZv8g%5sViTx4Gt8jrYyW8HpYJ$L5@t1Y!KxPWGGRzkpWTcSlhCuikH';
-  private googleSheetsUrl = 'https://sheet.best/api/sheets/4e7adda9-ae54-4fff-825d-00a3bfe790aa/tabs/registros';
+  private apiKey = 'uAM-8gJhDzeWx%splaFtIgMx-1i2XEkBATCzEv5ioade$Fbvm1$cbZUr0Fu4yEC0';
+  private googleSheetsUrl = 'https://sheet.best/api/sheets/38bd223f-a35a-4ae4-9136-c9e570aac1e0/tabs/registros';
   private dataToSave: any = {};
-  private apiKey1 = 'Sb$Ur08t_AZv8g%5sViTx4Gt8jrYyW8HpYJ$L5@t1Y!KxPWGGRzkpWTcSlhCuikH';
-  private googleSheetsUrl1 = 'https://sheet.best/api/sheets/4e7adda9-ae54-4fff-825d-00a3bfe790aa/tabs/contratista';
+  private apiKey1 = 'uAM-8gJhDzeWx%splaFtIgMx-1i2XEkBATCzEv5ioade$Fbvm1$cbZUr0Fu4yEC0';
+  private googleSheetsUrl1 = 'https://sheet.best/api/sheets/38bd223f-a35a-4ae4-9136-c9e570aac1e0/tabs/contratista';
   private personnelData: PersonnelEntry[] = [];
 
 
-  //     https://api.sheetbest.com/sheets/4e7adda9-ae54-4fff-825d-00a3bfe790aa
-//    'Sb$Ur08t_AZv8g%5sViTx4Gt8jrYyW8HpYJ$L5@t1Y!KxPWGGRzkpWTcSlhCuikH';
+  /*
+  private apiKey = 'uAM-8gJhDzeWx%splaFtIgMx-1i2XEkBATCzEv5ioade$Fbvm1$cbZUr0Fu4yEC0';
+  private connectionUrl = '38bd223f-a35a-4ae4-9136-c9e570aac1e0';
+*/
 
   constructor(
     private http: HttpClient,
