9fb9db60506266ea47f6f2f0ef37a8f7fa1b5e59
julian-castanoj <50122146+julian-castanoj@users.noreply.github.com>
2025-09-03 17:34:56 -0500

    Actualizar sheet.service.ts

--- src/app/consolidated-fortnight/services/sheet.service.ts
+++ src/app/consolidated-fortnight/services/sheet.service.ts
@@ -15,7 +15,7 @@ export class SheetsService {
   
 /*
   private apiKey = 'uAM-8gJhDzeWx%splaFtIgMx-1i2XEkBATCzEv5ioade$Fbvm1$cbZUr0Fu4yEC0';
-  private connectionUrl = 'https://sheet.best/api/sheets/38bd223f-a35a-4ae4-9136-c9e570aac1e0/tabs/vehiculos';
+  private connectionUrl = '38bd223f-a35a-4ae4-9136-c9e570aac1e0';
 */
 
   constructor(private http: HttpClient) {}
