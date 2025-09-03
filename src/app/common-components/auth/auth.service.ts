1ee28ea5c2984764ed26fd31c60a4c9b0d33dc41
julian-castanoj <50122146+julian-castanoj@users.noreply.github.com>
2025-09-03 17:29:24 -0500

    Actualizar auth.service.ts

--- src/app/common-components/auth/auth.service.ts
+++ src/app/common-components/auth/auth.service.ts
@@ -13,14 +13,16 @@ export interface AuthResponse {
   providedIn: 'root'
 })
 export class AuthService {
-  private googleSheetUrl = 'https://sheet.best/api/sheets/4e7adda9-ae54-4fff-825d-00a3bfe790aa/tabs/festivos';
+  private googleSheetUrl = 'https://sheet.best/api/sheets/38bd223f-a35a-4ae4-9136-c9e570aac1e0/tabs/festivos';
   private sessionTimeout: any; 
   private sessionDuration = 30 * 60 * 1000; 
 
 
 
-//     https://api.sheetbest.com/sheets/4e7adda9-ae54-4fff-825d-00a3bfe790aa
-//    Sb$Ur08t_AZv8g%5sViTx4Gt8jrYyW8HpYJ$L5@t1Y!KxPWGGRzkpWTcSlhCuikH
+/*
+  private apiKey = 'uAM-8gJhDzeWx%splaFtIgMx-1i2XEkBATCzEv5ioade$Fbvm1$cbZUr0Fu4yEC0';
+  private connectionUrl = 'https://sheet.best/api/sheets/38bd223f-a35a-4ae4-9136-c9e570aac1e0/tabs/vehiculos';
+*/
 
 
   constructor(private http: HttpClient, private router: Router, @Inject(PLATFORM_ID) private platformId: Object) {}
